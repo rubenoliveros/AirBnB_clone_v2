a089b7cd82dd69df29bad0879caab6730d743b58
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-05-02 17:20:15 -0500

    Adding def close

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,6 +45,10 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """Still don't know why"""
+        self.reload()
